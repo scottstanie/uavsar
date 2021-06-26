6b1fc5787f5e0df564c318cf290dcf4ea711c14e
scott <scott.stanie@gmail.com>
2021-06-26 08:24:23 -0400

    add setup for entry point

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+setuptools.setup(
+    name="query_uavsar",
+    version="0.1.0",
+    author="Scott Staniewicz",
+    author_email="scott.stanie@utexas.com",
+    description="Query and download UAVSAR PolSAR products",
+    url="https://github.com/scottstanie/uavsar",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: C",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
+    ],
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "uaquery=query_uavsar:cli",
+        ],
+    },
+    zip_safe=False,
+)
